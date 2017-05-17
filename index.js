e6e6a9f06bc1fffbcc0757db93df54bf13ce18c8
jinhoonbang <jinhoonbang@gmail.com>
2017-05-17 12:21:50 -0500

    commit

--- index.js
+++ index.js
@@ -3,14 +3,25 @@ import express from 'express';
 import path from 'path';
 import _http from 'http';
 import socketio from 'socket.io';
+import five from 'johnny-five';
 
 const SOCKET_PORT = 1234;
 const SERVER_PORT = 2222;
 const HOST = '127.0.0.1';
 
-let app = express();    
+let app = express();
 let http = _http.Server(app);
 let io = socketio(http);
+
+let b = new five.Board();
+
+b.on('ready', () => {
+  console.log("board connected")
+});
+
+// b.on('string',function(string){
+//   console.log('receiving string:' + string);
+// });
 
 app.locals.message = "";
 
@@ -25,19 +36,19 @@ io.on('connection', socket => {
   // })
 });
 
-let socket = dgram.createSocket('udp4');
-
-socket.on('listening', () => {
-  let address = socket.address();
-  console.log('UDP Server listening on ' + address.address + ":" + address.port);
-});
-
-socket.on('message', (message, remote) => {
-  console.log(remote.address + ':' + remote.port +' - ' + message);
-  io.emit('chat message', { "message" : message.toString() })
-});
-
-socket.bind(SOCKET_PORT, HOST);
+// let socket = dgram.createSocket('udp4');
+//
+// socket.on('listening', () => {
+//   let address = socket.address();
+//   console.log('UDP Server listening on ' + address.address + ":" + address.port);
+// });
+//
+// socket.on('message', (message, remote) => {
+//   console.log(remote.address + ':' + remote.port +' - ' + message);
+//   io.emit('chat message', { "message" : message.toString() })
+// });
+//
+// socket.bind(SOCKET_PORT, HOST);
 
 http.listen(SERVER_PORT, () => {
   console.log('Listening on port' + SERVER_PORT);
